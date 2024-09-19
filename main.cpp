66206e42fec838fa97e2c1e02796405147ec2a59
XimePerezEscalante <142125448+XimePerezEscalante@users.noreply.github.com>
2024-09-19 00:15:04 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,19 +6,29 @@
 
 
 int main(int argc, const char * argv[]) {
-    std::vector<Cancion> listaCanciones = {Cancion("Beloved","Celeste","Baladas",3.58),Cancion("Angel of my Dreams","JADE","Pop",3.18),
-  Cancion("Stealin' Love","Leigh-Anne","Pop",2.20),Cancion("Tears","Perrie","Pop",2.55),Cancion("Hold On","The Internet","R&B/Soul",6.46),
-  Cancion("Only Human","SiR","R&B/Soul",4.25),Cancion("the fear is not real","WILLOW","Contemporary Jazz",3.38),
-  Cancion("How Many Mics","The Fugees","Hip-Hop/Rap",4.29),Cancion("CPU","Raury","Alternativa",4.29),
-  Cancion("DOWN","blackbear","R&B/Soul",2.20),Cancion("mirrored heart","FKA twigs","Electrónica",4.33)};
+    std::vector<Cancion> listaCanciones = {Cancion("Beloved","Celeste","Baladas",3.58),
+    Cancion("Angel of my Dreams","JADE","Pop",3.18),Cancion("Stealin' Love","Leigh-Anne","Pop",2.20),
+    Cancion("Tears","Perrie","Pop",2.55),Cancion("Hold On","The Internet","R&B/Soul",6.46),
+    Cancion("Only Human","SiR","R&B/Soul",4.25),
+    Cancion("the fear is not real","WILLOW","Contemporary Jazz",3.38),
+    Cancion("How Many Mics","The Fugees","Hip-Hop/Rap",4.29),
+    Cancion("Cpu","Raury","Alternativa",4.29),
+    Cancion("Down","Blackbear","R&B/Soul",2.20),
+    Cancion("mirrored heart","FKA twigs","Electrónica",4.33)};
     
     Biblioteca<Cancion> bibliotecaUsuario(listaCanciones);
     
-    bibliotecaUsuario.ordenArtista("Raury");
+    bibliotecaUsuario.ordenArtista();
     
     std::cout << bibliotecaUsuario.toString() << std::endl;
     
-    std::cout << "XPE - Reproductor de Música:\n1. Ordenar por Artista\n2. Ordenar por Género\n3. Ordenar por Duración" << std::endl;
-  
+    bibliotecaUsuario.ordenGenero();
+    
+    std::cout << bibliotecaUsuario.toString() << std::endl;
+    
+    bibliotecaUsuario.ordenDuracion();
+    
+    std::cout << bibliotecaUsuario.toString() << std::endl;
+    
     return 0;
 }
