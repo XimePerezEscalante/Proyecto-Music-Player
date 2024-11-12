7cdf6f5c8944be63ad9007a61011485d7998074a
XimePerezEscalante <142125448+XimePerezEscalante@users.noreply.github.com>
2024-11-11 23:27:07 -0600

    Update Cancion.h

--- Cancion.h
+++ Cancion.h
@@ -1,47 +1,79 @@
+//
+//  Cancion.h
+//  MusicPlayer
+//
+//  Created by Ximena Perez Escalante on 18/09/24.
+//
+
 #ifndef Cancion_h
 #define Cancion_h
+
+#include <iostream>
+#include <sstream>
+
+// template <class T>
+// class FilaReproduccion;
 
 template <class T>
 class ListaReproduccion;
 
-class Cancion{
-    private:
+class Cancion
+{
+private:
     std::string titulo;
     std::string artista;
     std::string genero;
     float duracion;
+
 public:
+    // Constructores
     Cancion();
-    Cancion(std::string,std::string,std::string);
-    Cancion(std::string,std::string,std::string,float);
-    std::string getTitulo(){return titulo;};
-    std::string getArtista(){return artista;};
-    std::string getGenero(){return genero;};
-    float getDuracion(){return duracion;};
-    
+    Cancion(std::string, std::string, std::string);
+    Cancion(std::string, std::string, std::string, float);
+    // Getters
+    std::string getTitulo() { return titulo; };
+    std::string getArtista() { return artista; };
+    std::string getGenero() { return genero; };
+    float getDuracion() { return duracion; };
+
+    std::string imprimirCancion();
+
     friend class ListaReproduccion<Cancion>;
+    // friend class FilaReproduccion<Cancion>;
 };
 
-
-Cancion::Cancion(){
+// Constructor por omisión
+Cancion::Cancion()
+{
     titulo = "Sin nombre";
     artista = "Desconocido";
     genero = "Desconocido";
     duracion = 0.0;
 }
 
-
-Cancion::Cancion(std::string title,std::string artist,std::string genre){
+// Constructor con parámetros (menos la duración)
+Cancion::Cancion(std::string title, std::string artist, std::string genre)
+{
     titulo = title;
     artista = artist;
     genero = genre;
     duracion = 2.5;
 }
 
-Cancion::Cancion(std::string title,std::string artist,std::string genre,float length){
+// Constructor con todos los parámetros
+Cancion::Cancion(std::string title, std::string artist, std::string genre, float length)
+{
     titulo = title;
     artista = artist;
     genero = genre;
     duracion = length;
 }
+
+std::string Cancion::imprimirCancion()
+{
+    std::stringstream aux;
+    aux << titulo << " - " << artista << " - " << genero << " - " << duracion;
+    return aux.str();
+}
+
 #endif /* Cancion_h */
